398924f01a982496c5752be94c2adc02fe899c49
Sam Ashtar <samashtarm@gmail.com>
2018-06-27 19:47:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function add2(n) {
   const two = 2
 }
 
-var funkyFunction = function(funkyFunction) {
-  return function(funkyFunction) {
+var funkyFunction = function() {
+  return function() {
     return "FUNKY!"
     console.log (funkyFunction ())
   }
